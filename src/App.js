82a4573f69d6d0cfd0129f71444085d7dbfbf0ac
Chris Boutiette <cboutiette@gmail.com>
2019-12-14 17:11:23 -0500

    INIT create react app for my site

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './chris_2_round.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Chris Boutiette dot com
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
